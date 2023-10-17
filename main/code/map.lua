a6fe2f08b7dfe4b97f175c629e14c536fa8e44ec
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-10-17 08:04:22 -0400

    move spawn tile and add draw map

--- main/code/map.lua
+++ main/code/map.lua
@@ -1,4 +1,5 @@
 local table_utility = require("libraries/table_utility/table_utility")
+local hex = require("libraries/hexagon/hexagon")
 
 local M = {}
 
@@ -13,7 +14,29 @@ local maps = {}
 
 --- level { [n] { q, r } }
 local map = {}
-   
+
+local factory_tile = "/factory_tile#tile"
+
+local function draw_tile_sprite(spawn_position)
+   return factory.create(factory_tile, spawn_position)
+end
+
+local function remove_tile_sprite(tile_hash)
+   go.delete(tile_hash)
+end
+
+----------------------------------------
+-- -- -- --'MAPS'-- -- -- --
+----------------------------------------
+local function clear_current_map()
+   M.current_map = {}
+   M.current_coord_map = {}
+end  
+
+local function load_map(level)
+   return map[level]
+end
+
 function M.save_current_map(level) -- level
    local index = 0
    map[level] = {}
@@ -22,14 +45,6 @@ function M.save_current_map(level) -- level
    end
 end
 
-function M.clear_current_map()
-   M.current_map = {}
-   M.current_coord_map = {}
-end
-
-function M.load_map(level)
-   return map[level]
-end
 
 function M.get_levels_amount()
    return #map or 1
@@ -44,17 +59,26 @@ function M.save_profile() --(profile)
    end
 end
 
-function M.add_tile(q, r, tile_hash)
+
+
+----------------------------------------
+-- -- -- --'TILE'-- -- -- --
+----------------------------------------
+
+function M.add_tile(q, r)
    if M.current_coord_map[q] == nil then M.current_coord_map[q] = {} end
+   local tile_hash = draw_tile_sprite(hex.flat_hex_to_pixel(q, r))
    table.insert(M.current_map, {q, r})
    M.current_coord_map[q][r] = {tile_hash, #M.current_map}
 end
 
-function M.get_tile_hash(q, r)
+local function get_tile_hash(q, r)
    return M.current_coord_map[q][r][1]
 end
 
 function M.remove_tile(q, r)
+   local tile_hash = get_tile_hash(q, r)
+   remove_tile_sprite(tile_hash)
    local tile_id = M.current_coord_map[q][r][2]
    table_utility.remove_element(M.current_map, tile_id)
    M.current_coord_map[q][r] = 0
@@ -65,6 +89,19 @@ function M.has_tile(q, r)
       if M.current_coord_map[q][r] ~= nil and M.current_coord_map[q][r] ~= 0 then return true end
    end
    return false
+end
+
+----------------------------------------
+-- -- -- --'DRAW MAP'-- -- -- --
+----------------------------------------
+
+function M.draw_map(level)
+   clear_current_map()
+   local new_map = load_map(level)
+   for i = 1, #new_map do 
+      local q, r = new_map[i][1], new_map[i][2]
+      M.add_tile(q, r)
+   end
 end
 
 
