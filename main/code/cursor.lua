2d9ad3f20b58012b20115f2b1c29df3202d06dad
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-10-18 04:32:35 -0400

    add level buttons

--- main/code/cursor.lua
+++ main/code/cursor.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+local cursor_sprite = "/cursor#sprite"
 M.free_cursor_mode = true
+
+function M.turn_off_free_mode()
+   M.free_cursor_mode = false
+   sprite.play_flipbook(cursor_sprite, hash("empty"))
+   defos.set_cursor_visible(true)
+end
+
+function M.turn_on_free_mode()
+   M.free_cursor_mode = true
+   sprite.play_flipbook(cursor_sprite, hash("tile_selection"))
+   defos.set_cursor_visible(false)
+end
 
 return M
