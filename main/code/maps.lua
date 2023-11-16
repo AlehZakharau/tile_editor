f6e252833c467e020d72bd89b5ca6c3d9bf86e6c
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-11-16 14:14:59 -0500

    open map profile

--- main/code/maps.lua
+++ main/code/maps.lua
@@ -14,14 +14,17 @@ M.current_map = "level_1"
 ----------------------------------------
 
 local function add_defalt_level()
-   M.maps[M.current_profile] =  { ["level_1"] = {} }
+   M.maps[M.current_profile] =  { ["level_1"] = { } }
    M.current_map = "level_1"
+   messanger.push_notification(hash_table.map_updated)
 end
 
 local function add_defalt_profile()
    M.maps["defalt"] = { ["level_1"] = {} } 
    M.current_profile = "defalt"
    M.current_map = "level_1"
+   messanger.push_notification(hash_table.map_updated)
+   messanger.push_notification(hash_table.profile_updated)
 end
 
 ----------------------------------------
@@ -35,6 +38,9 @@ end
 function M.add_profile(profile_name)
    M.maps[profile_name] = { ["level_1"] = {} }
    M.current_profile = profile_name
+   M.current_map = "level_1"
+   messanger.push_notification(hash_table.profile_updated)
+   messanger.push_notification(hash_table.map_updated)
 end
 
 function M.remove_profile(profile_name)
@@ -46,7 +52,7 @@ function M.remove_profile(profile_name)
       if #profiles == 0 then
          add_defalt_profile()
       else
-         M.current_profile = profiles[1]
+         M.open_profile(profiles[1])
       end
    end
 end
@@ -57,6 +63,14 @@ function M.rename_profile(profile_name, new_name)
    end
    M.maps[new_name] = M.maps[profile_name]
    M.remove_profile(profile_name)
+   messanger.push_notification(hash_table.profile_updated)
+end
+
+function M.open_profile(profile_name)
+   M.current_profile = profile_name
+   messanger.push_notification(hash_table.profile_updated)
+   map = M.get_first_map(profile_name)
+   M.open_map(map)
 end
 
 function M.get_profiles()
@@ -71,9 +85,16 @@ function M.get_maps(profile_name)
    return M.maps[profile_name]
 end
 
+function M.get_first_map(profile_name)
+   for k, v in pairs(M.maps[profile_name]) do
+      return k
+   end
+end
+
 function M.add_map(map_name)
-   M.maps[M.current_profile][map_name] = {}
+   M.maps[M.current_profile][map_name] = {map_name = map_name}
    M.current_map = map_name
+   messanger.push_notification(hash_table.map_updated)
 end
 
 function M.remove_map(map_name)
@@ -86,6 +107,7 @@ function M.remove_map(map_name)
          add_defalt_level()
       else
          M.current_map = maps[1]
+         messanger.push_notification(hash_table.map_updated)
       end
    end
 end
@@ -96,10 +118,16 @@ function M.rename_map(map_name, new_name)
    end
    M.maps[M.current_profile][new_name] = M.maps[M.current_profile][map_name]
    M.remove_map(map_name)
+   messanger.push_notification(hash_table.map_updated)
+end
+
+function M.open_map(map_name)
+   M.current_map = map_name
+   messanger.push_notification(hash_table.map_updated)
 end
 
 function M.print_maps()
-   print("Current profile: " .. M.current_profile)
+   print("Current profile: " .. M.current_profile .. "::: Current map:" .. M.current_map)
    pprint(M.maps)
 end
 
--- main/hash_table.lua
+++ main/hash_table.lua
@@ -3,5 +3,7 @@ local M = {}
 M.menu = "/menu#menu"
 
 M.tile_spawned = hash("tile_spawned")
+M.map_updated = hash("map_updated")
+M.profile_updated = hash("profile_updated")
 
 return M
