500c22089a1096ef030cef41b059fefa00712fe8
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2024-03-28 19:29:16 -0400

    pointy factory

--- prefabs/fabrics/factory_tile.go
+++ prefabs/fabrics/factory_tile.go
@@ -1,6 +1,21 @@
 components {
-  id: "tile"
-  component: "/prefabs/fabrics/tile.factory"
+  id: "tile_flat"
+  component: "/prefabs/fabrics/tile_flat.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "tile_pointy"
+  component: "/prefabs/fabrics/tile_pointy.factory"
   position {
     x: 0.0
     y: 0.0
---
+++ prefabs/tile_pointy.go
@@ -0,0 +1,30 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"tile\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "size {\n"
+  "  x: 76.8\n"
+  "  y: 66.75\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/ui.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
