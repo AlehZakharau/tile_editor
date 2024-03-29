6b2c11543b93ff57fbf1185eb4208ca72328797f
Aleh Zakharau <Alehzaharau@tutanota.com>
2024-03-29 17:46:15 -0400

    add tile's size and orintation test

---
+++ main/tests/tile_tests.lua
@@ -0,0 +1,19 @@
+local console = require("defcon.console")
+local hex = require("lib.hexagon.hexagon")
+
+local M = {}
+
+function M.add_tile_commands()
+	console.register_command("update_tile_size", "w", 
+	function (argm) 
+		hex.change_size(argm[1])
+		return
+	end)
+	console.register_command("update_tile_orintation", "w",
+	function (argm)
+		hex.change_orintation(argm[1])
+		return
+	end)
+end
+
+return M
