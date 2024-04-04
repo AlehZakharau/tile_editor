0c062fa410a68174c00297a3bbe3815193222ed1
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2024-04-04 13:15:13 -0400

    fix orintation

--- main/tests/tile_tests.lua
+++ main/tests/tile_tests.lua
@@ -3,15 +3,17 @@ local hex = require("lib/hexagon/hexagon")
 
 local M = {}
 
+local string_to_bool={ ["true"]=true, ["false"]=false }
 function M.add_tile_commands()
 	console.register_command("update_tile_size", "w", 
 	function (argm) 
-		hex.change_size(argm[1])
+		hex.change_size(tonumber(argm[1]))
 		return
 	end)
 	console.register_command("update_tile_orintation", "w",
 	function (argm)
-		hex.change_orintation(argm[1])
+      local value = string_to_bool[argm[1]]
+		hex.change_orintation(value)
 		return ""
 	end)
 end
